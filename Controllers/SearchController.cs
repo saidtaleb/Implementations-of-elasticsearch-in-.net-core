becd329d03c10ef7420cd81d923970a19426b386
Taleb Said <saidtaleb128@gmail.com>
2019-04-06 21:03:34 +0100

    Project files

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,70 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MiniBlogElasticsearch;
+using MiniBlogElasticsearch.Models;
+using Nest;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class SearchController : Controller
+{
+    private readonly IBlogService _blogService;
+    private readonly IElasticClient _elasticClient;
+    private readonly IOptionsSnapshot<BlogSettings> _settings;
+
+    public SearchController(IBlogService blogService, IElasticClient elasticClient, IOptionsSnapshot<BlogSettings> settings)
+    {
+        _blogService = blogService;
+        _elasticClient = elasticClient;
+        _settings = settings;
+    }
+
+    [Authorize]
+    [Route("/search/reindex")]
+    public async Task<IActionResult> ReIndex()
+    {
+        await _elasticClient.DeleteByQueryAsync<Post>(q => q.MatchAll());
+
+        var allPosts = (await _blogService.GetPosts(int.MaxValue)).ToArray();
+
+        foreach (var post in allPosts)
+        {
+            await _elasticClient.IndexDocumentAsync(post);
+        }
+
+        return Ok($"{allPosts.Length} post(s) reindexed");
+    }
+
+    [Route("/search")]
+    public async Task<IActionResult> Find(string query, int page = 1, int pageSize = 5)
+    {
+        var response = await _elasticClient.SearchAsync<Post>(
+            s => s.Query(q => q.QueryString(d => d.Query(query)))
+                .From((page - 1) * pageSize)
+                .Size(pageSize));
+
+        ViewData["Title"] = _settings.Value.Name + " - Search Results";
+        ViewData["Description"] = _settings.Value.Description;
+
+        if (!response.IsValid)
+        {
+            // We could handle errors here by checking response.OriginalException or response.ServerError properties
+            return View("Results", new Post[] { });
+        }
+
+        if (page > 1)
+            ViewData["prev"] = GetSearchUrl(query, page - 1, pageSize);
+        if (response.IsValid && response.Total > page * pageSize)
+            ViewData["next"] = GetSearchUrl(query, page + 1, pageSize);
+
+        return View("Results", response.Documents);
+    }
+
+    private static string GetSearchUrl(string query, int page, int pageSize)
+    {
+        return $"/search?query={Uri.EscapeDataString(query ?? "")}&page={page}&pagesize={pageSize}/";
+    }
+}
---
+++ Extension/ElasticsearchExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MiniBlogElasticsearch.Models;
+using Nest;
+using System;
+
+public static class ElasticsearchExtensions
+{
+    public static void AddElasticsearch(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        var url = configuration["elasticsearch:url"];
+        var defaultIndex = configuration["elasticsearch:index"];
+
+        var settings = new ConnectionSettings(new Uri(url))
+            .DefaultIndex(defaultIndex)
+            .DefaultMappingFor<Post>(m => m
+                .Ignore(p => p.IsPublished)
+                .PropertyName(p => p.ID, "id")
+            )
+            .DefaultMappingFor<Comment>(m => m
+                .Ignore(c => c.Email)
+                .Ignore(c => c.IsAdmin)
+                .PropertyName(c => c.ID, "id")
+            );
+
+        var client = new ElasticClient(settings);
+
+        services.AddSingleton<IElasticClient>(client);
+    }
+}
